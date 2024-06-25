7d6c707fd69c538dea87e4b24513dbc22a91d0df
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-25 17:16:51 +0200

    created new <P> whit methods to replace words

--- index.php
+++ index.php
@@ -7,19 +7,32 @@
 </head>
 <body>
     <main>
+        <p>
         <?php
         $textPhar = 'Marx e Engels analizzano la storia come lotta di classe, sempre esistita e combattuta tra oppressi e oppressori. I due sottolineano come questo contrasto non solo sia ancora presente nella moderna società borghese, ma che piuttosto si sia addirittura inasprito, poiché in seguito a grandi trasformazioni sociali connesse alla trasformazione del modello produttivo è animato da solo due grandi classi: la borghesia e il proletariato. La prima, classe rivoluzionaria nel Basso Medioevo e all inizio dell età moderna, dopo aver annientato la struttura economica e politica allora esistente, ormai inadeguata e obsoleta, si consacrò come classe dominante a tutti gli effetti durante la rivoluzione industriale. La seconda, nata in seguito alla nascita del modello economico capitalistico, risulta essere quella oppressa, ma potenzialmente dominante.';
         echo $textPhar;
-        
         ?>
-        <h2>lunghezza  <?php echo strlen($textPhar); ?> </h2>
-
+        </p>
+        
+        <h3>lunghezza  <?php echo strlen($textPhar); ?> </h3>
+        
+        <h3>ciao <?php echo $_GET['change']?> </h3>
+        
         <form action="./index.php" method='GET'>
             <input type="text" name='change' id='change'>
             <button type='submit'>
                 send
             </button>
         </form>
+        
+        <p>
+            <?php
+        $textPhar = 'Marx e Engels analizzano la storia come lotta di classe, sempre esistita e combattuta tra oppressi e oppressori. I due sottolineano come questo contrasto non solo sia ancora presente nella moderna società borghese, ma che piuttosto si sia addirittura inasprito, poiché in seguito a grandi trasformazioni sociali connesse alla trasformazione del modello produttivo è animato da solo due grandi classi: la borghesia e il proletariato. La prima, classe rivoluzionaria nel Basso Medioevo e all inizio dell età moderna, dopo aver annientato la struttura economica e politica allora esistente, ormai inadeguata e obsoleta, si consacrò come classe dominante a tutti gli effetti durante la rivoluzione industriale. La seconda, nata in seguito alla nascita del modello economico capitalistico, risulta essere quella oppressa, ma potenzialmente dominante.';
+        $textPhar= str_replace('change','(***)', $textPhar);
+        echo $textPhar;
+        ?>
+        <h3>lunghezza  <?php echo strlen($textPhar); ?> </h3>
+        </p>
     </main>
 </body>
 </html>
